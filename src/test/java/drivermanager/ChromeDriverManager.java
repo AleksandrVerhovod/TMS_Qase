6efa5ebb86f0028aa35d8783f4adbf5419009276
Aleksandr <verhovod.aleksandr@gmail.com>
2022-10-02 18:25:05 +0300

    Allure added

--- src/test/java/drivermanager/ChromeDriverManager.java
+++ src/test/java/drivermanager/ChromeDriverManager.java
@@ -12,7 +12,7 @@ public class ChromeDriverManager extends DriverManager {
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-notifications");
-//        options.addArguments("--headless"); //фоновый режим
+        options.addArguments("--headless"); //фоновый режим
         options.setExperimentalOption("excludeSwitches",
                 Arrays.asList("disable-popup-blocking"));
         threadLocalDriver.set(new ChromeDriver(options));
