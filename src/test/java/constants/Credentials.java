1309cbe104006856ba5af92066ff466ca8a9267b
Aleksandr <verhovod.aleksandr@gmail.com>
2022-10-11 11:55:20 +0300

    added credentials

---
+++ src/test/java/constants/Credentials.java
@@ -0,0 +1,16 @@
+package constants;
+
+public class Credentials {
+    public static final String EMAIL_VALID = "verhovod.aleksandr@gmail.com";
+    public static final String PASSWORD_VALID = "0123456789";
+
+
+    public static final String EMAIL_NO_CONFIRM = "fippeutajurei-3971@yopmail.com";
+    public static final String PASSWORD_NO_CONFIRM = "1234567890";
+
+
+    public static final String DEMO_QA_PROJECT = "DemoQAProject";
+
+
+
+}
